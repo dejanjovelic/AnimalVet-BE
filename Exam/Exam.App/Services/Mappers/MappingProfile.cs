545c073bd4e070272c0aab710fba8239c0166fd8
dejan.jovelic <dejan.jovelic@gmail.com>
2026-02-18 15:49:45 +0100

    feat: inicijalizacija projekta

---
+++ Exam/Exam.App/Services/Mappers/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Exam.App.Domain;
+using Exam.App.Services.Dtos;
+
+namespace Exam.App.Services.Mappers
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ApplicationUser, ProfileDto>();
+        }
+    }
+}
